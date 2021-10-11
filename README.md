04b6085bf337ece7d0cef0c32ec478ed5658e6ab
diegograsso <diego.grasso@gmail.com>
2021-10-11 00:59:27 -0300

    Services v0.1 (#1)
    * First commit
    
    * Document
    
    * Lint
    
    Co-authored-by: Diego Grasso <diegograsso@iovlab.org>
    Co-authored-by: Ilan <ilanolkies@outlook.com>

--- README.md
+++ README.md
@@ -20,4 +20,74 @@
   </a>
 </p>
 
+RIF Wallet Services provide a basic account querying API to bootstrap your app.
+
+## Usage
+
+The API is as follows. Fin the response types in `src/types.ts`
+
+> Use addresses in lower case
+
+### Tokens
+
+Get the list of all the tokens in the RSK network
+
+```
+GET /tokens -> IToken[]
+```
+
+### Addresses
+
+Get the tokens of an address:
+
+
+```
+GET /address/:address/tokens -> ITokenWithBalance[]
+```
+
+Get the events related to a given address
+
+```
+GET /address/:address/events -> IEvent[]
+```
+
+Get the incoming and outgoing transactions of a given address
+
+```
+GET /address/:address/transactions -> IApiTransactions[]
+```
+
 ## Run for development
+
+Install dependencies:
+
+```
+npm i
+```
+
+Run tests:
+
+```
+npm test
+npm test:watch
+```
+
+Lint the code:
+
+```
+npm run lint
+npm run lint:fix
+```
+
+Start the service:
+
+```
+npm run start
+npm run start:prod
+```
+
+### Branching model
+
+- `main` has latest release. PRs need to pass `ci`. Merge into `main` will deploy to npm. Do merge commits.
+- Use branches pointing to `main` to add new PRs.
+- Do external PRs against latest commit in `main`.
---
+++ src/index.ts
@@ -0,0 +1,55 @@
+import 'dotenv/config'
+import express, { Request, Response } from 'express'
+import { Api } from './api'
+import { isValidAddress } from './utils'
+
+const environment = { // TODO: remove these defaults
+  API_URL: process.env.API_URL as string || 'https://backend.explorer.testnet.rsk.co/api',
+  PORT: parseInt(process.env.PORT as string) || 3000,
+  CHAIN_ID: parseInt(process.env.CHAIN_ID as string) || 31
+}
+
+const app = express()
+const api = new Api(environment.API_URL, environment.CHAIN_ID)
+
+app.listen(environment.PORT, () => {
+  console.log(`RIF Wallet services running on port ${environment.PORT}.`)
+})
+
+app.get('/tokens', async (request: Request, response: Response) => {
+  console.log(request.path)
+  response.status(200).json(await api.getTokens())
+})
+
+app.get('/address/:address/tokens', async (request: Request, response: Response) => {
+  console.log(request.path)
+  const address = request.params.address
+  if (!address) return response.status(404)
+  if (isValidAddress(address)) {
+    response.status(200).json(await api.getTokensByAddress(address))
+  } else {
+    response.status(400).send('Invalid address')
+  }
+})
+
+app.get('/address/:address/events', async (request: Request, response: Response) => {
+  console.log(request.path)
+  const address = request.params.address
+  if (!address) return response.status(404)
+  if (isValidAddress(address)) {
+    response.status(200).json(await api.getEventsByAddress(address))
+  } else {
+    response.status(400).send('Invalid address')
+  }
+})
+
+app.get('/address/:address/transactions', async (request: Request, response: Response) => {
+  console.log(request.path)
+  const address = request.params.address
+  if (!address) return response.status(404)
+  if (isValidAddress(address)) {
+    response.status(200).json(await api.getTransactionsByAddress(address))
+  } else {
+    response.status(400).send('Invalid address')
+  }
+})
