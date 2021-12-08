2fe5713519076d3ea10e78a1638ddb07c7d1d4a1
Agustin Villalobos V <agustinvillalobos@protonmail.com>
2021-12-08 13:52:42 -0600

    CoinMarketCap class to access the quotas(prices) of each token or coin

---
+++ src/coinmarketcap.ts
@@ -0,0 +1,35 @@
+import axios from "axios";
+
+import { ICoinMarketCapResponse, ICryptocurrency, IQuoteParams } from "./types";
+
+export class CoinMarketCap {
+  apiKey: string;
+  version: string;
+  baseURL: string;
+
+  constructor(
+    apiKey: string,
+    baseURL = "https://pro-api.coinmarketcap.com",
+    version = "v1"
+  ) {
+    this.apiKey = apiKey;
+    this.baseURL = baseURL;
+    this.version = version;
+  }
+
+  async getQuotesLatest(params?: IQuoteParams) {
+    try {
+      const { data } = await axios.get<
+        ICoinMarketCapResponse<Record<string, ICryptocurrency>>
+      >(`${this.baseURL}/${this.version}/cryptocurrency/quotes/latest`, {
+        params,
+        headers: {
+          "X-CMC_PRO_API_KEY": this.apiKey,
+        },
+      });
+      return data;
+    } catch (error) {
+      throw error;
+    }
+  }
+}
