b8e0dac9e6f121cd23721e8279222e87a56f8556
Christian Escalante <chescalante@gmail.com>
2021-12-14 14:09:47 -0300

    better emit event name

--- src/subscriptions/pushNewBalances.ts
+++ src/subscriptions/pushNewBalances.ts
@@ -9,7 +9,7 @@ const pushNewBalances = async (
   const balances = await api.getBalances(address.toLowerCase())
 
   for (const balance of balances.data) {
-    socket.emit('something', { type: 'newBalance', payload: balance })
+    socket.emit('change', { type: 'newBalance', payload: balance })
   }
 }
 
