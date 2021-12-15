2b360275dcba963c342cfd12889f977b34af9744
Christian Escalante <chescalante@gmail.com>
2021-12-15 10:23:55 -0300

    push new balances working

--- src/subscriptions/pushNewBalances.ts
+++ src/subscriptions/pushNewBalances.ts
@@ -2,14 +2,49 @@ import { Socket } from 'socket.io'
 import { DefaultEventsMap } from 'socket.io/dist/typed-events'
 import { Api } from '../api'
 
+interface ISendedBalances {
+  [address: string]: {
+    [tokenAddress: string]: string
+  }
+}
+
+const EXECUTION_INTERVAL = 60000
+
+const sendedBalances: ISendedBalances = {}
+
 const pushNewBalances = async (
   socket: Socket<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap, any>,
-  api: Api, address: string
+  api: Api,
+  address: string
 ) => {
-  const balances = await api.getBalances(address.toLowerCase())
+  const execute = executeFactory(socket, api, address)
 
-  for (const balance of balances.data) {
-    socket.emit('change', { type: 'newBalance', payload: balance })
+  execute()
+
+  const timer = setInterval(execute, EXECUTION_INTERVAL)
+
+  return () => {
+    clearInterval(timer)
+    sendedBalances[address] = {}
+  }
+}
+
+const executeFactory = (
+  socket: Socket<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap, any>,
+  api: Api,
+  address: string
+) => async () => {
+  if (!sendedBalances[address]) {
+    sendedBalances[address] = {}
+  }
+
+  const tokens = await api.getTokensByAddress(address.toLowerCase())
+
+  for (const token of tokens) {
+    if (sendedBalances[address][token.contractAddress] !== token.balance) {
+      sendedBalances[address][token.contractAddress] = token.balance
+      socket.emit('change', { type: 'newBalance', payload: token })
+    }
   }
 }
 
