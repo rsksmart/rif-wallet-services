ce6a3078d5abdf8cc0c320a92aa96e136adbe2b5
Ilan <36084092+ilanolkies@users.noreply.github.com>
2021-12-20 15:15:20 -0300

    Feature/us 444 token price refactor (#10)
    * Move parsing logic to coinmarketcap.ts
    
    * Move files
    
    * Refactor api
    
    * Lint
    
    * Refactor json result
    
    * Add tests
    
    * Add error handling tests - move validatoins to api

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import 'dotenv/config'
-import express, { Request, Response } from 'express'
-import { Api } from './api'
-import { CoinMarketCap } from './coinmarketcap'
-import registeredDapps from './registered_dapps'
-import { isValidAddress, sanitizeMetadataResponse, sanitizeQuotaResponse } from './utils'
+import express from 'express'
 
-import { IPricesQuery } from './types'
+import axios from 'axios'
+import { CoinMarketCapAPI } from './coinmatketcap'
+import { RSKExplorerAPI } from './rskExplorerApi'
+import { registeredDapps } from './registered_dapps'
+import { setupApi } from './api'
 
 const environment = {
   // TODO: remove these defaults
@@ -14,81 +14,23 @@ const environment = {
     'https://backend.explorer.testnet.rsk.co/api',
   PORT: parseInt(process.env.PORT as string) || 3000,
   CHAIN_ID: parseInt(process.env.CHAIN_ID as string) || 31,
-  COIN_MARKET_CAP_KEY: `${process.env.COIN_MARKET_CAP_KEY}`
+  COIN_MARKET_CAP_URL: process.env.COIN_MARKET_CAP_URL as string || 'https://pro-api.coinmarketcap.com',
+  COIN_MARKET_CAP_VERSION: process.env.COIN_MARKET_CAP_VERSION as string || 'v1',
+  COIN_MARKET_CAP_KEY: process.env.COIN_MARKET_CAP_KEY! as string
 }
 
 const app = express()
-const api = new Api(environment.API_URL, environment.CHAIN_ID)
-const coinMarketCap = new CoinMarketCap(environment.COIN_MARKET_CAP_KEY)
+
+const rskExplorerApi = new RSKExplorerAPI(environment.API_URL, environment.CHAIN_ID, axios)
+const coinMarketCapApi = new CoinMarketCapAPI(environment.COIN_MARKET_CAP_URL, environment.COIN_MARKET_CAP_VERSION, environment.COIN_MARKET_CAP_KEY, axios)
+
+setupApi(app, {
+  rskExplorerApi,
+  coinMarketCapApi,
+  registeredDapps,
+  logger: console
+})
 
 app.listen(environment.PORT, () => {
   console.log(`RIF Wallet services running on port ${environment.PORT}.`)
-})
-
-app.get('/tokens', async (request: Request, response: Response) => {
-  console.log(request.path)
-  response.status(200).json(await api.getTokens())
-})
-
-app.get('/address/:address/tokens', async (request: Request, response: Response) => {
-  console.log(request.path)
-  const address = request.params.address
-  if (!address) return response.status(404)
-  if (isValidAddress(address)) {
-    response.status(200).json(await api.getTokensByAddress(address))
-  } else {
-    response.status(400).send('Invalid address')
-  }
-})
-
-app.get('/address/:address/events', async (request: Request, response: Response) => {
-  console.log(request.path)
-  const address = request.params.address
-  if (!address) return response.status(404)
-  if (isValidAddress(address)) {
-    response.status(200).json(await api.getEventsByAddress(address))
-  } else {
-    response.status(400).send('Invalid address')
-  }
-})
-
-app.get('/address/:address/transactions', async (request: Request, response: Response) => {
-  console.log(request.path)
-  const address = request.params.address
-  const { limit, prev, next } = request.query
-
-  if (!address) return response.status(404)
-  if (isValidAddress(address)) {
-    const result = await api.getTransactionsByAddress(address, limit as string, prev as string, next as string)
-    response.status(200).json(result)
-  } else {
-    response.status(400).send('Invalid address')
-  }
-})
-
-app.get('/price', async (request: Request<{}, {}, {}, IPricesQuery>, response: Response) => {
-  const fiat = request.query.fiat.toUpperCase()
-  const { tokens } = request.query
-
-  const tokensArray = tokens.split(',')
-  const metadataPromise = tokensArray.map(
-    async (address) => await coinMarketCap.getMetadata({ address: address })
-  )
-
-  try {
-    const coinMarketCapIdsResult = await Promise.all(metadataPromise)
-    const coinMarketCapIds = coinMarketCapIdsResult.map((response) => sanitizeMetadataResponse(response))
-
-    const quotes = await coinMarketCap.getQuotesLatest({ convert: fiat, id: coinMarketCapIds.join(',') })
-    const sanitizedTokenPrices = sanitizeQuotaResponse(quotes, fiat)
-      .map((token, idx) => ({ ...token, address: tokensArray[idx] }))
-
-    response.status(200).send(sanitizedTokenPrices)
-  } catch (error) {
-    response.status(500).send('Internal error')
-  }
-})
-
-app.get('/dapps', async (request: Request, response: Response) => {
-  response.status(200).json(registeredDapps)
 })
