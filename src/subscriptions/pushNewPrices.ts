d5ceeb228dc7b39f2f74c72eeb7ab128dd3e722f
Agustin Villalobos <agustin.villalobos@iovlabs.org>
2021-12-21 09:51:36 -0600

    Polling function for coinmarketcap prices

---
+++ src/subscriptions/pushNewPrices.ts
@@ -0,0 +1,37 @@
+import { CoinMarketCapAPI } from '../coinmatketcap'
+import { DefaultEventsMap } from 'socket.io/dist/typed-events'
+import { RSKExplorerAPI } from '../rskExplorerApi/index'
+import { Socket } from 'socket.io'
+
+const EXECUTION_INTERVAL = 60000
+
+const pushNewPrices = (socket: Socket<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap, any>,
+  api: RSKExplorerAPI,
+  cmc: CoinMarketCapAPI,
+  address: string,
+  convert: string
+) => {
+  const execute = getPricesByToken(socket, api, cmc, address, convert)
+
+  execute()
+
+  const timer = setInterval(execute, EXECUTION_INTERVAL)
+
+  return () => {
+    clearInterval(timer)
+  }
+}
+
+const getPricesByToken = (
+  socket: Socket<DefaultEventsMap, DefaultEventsMap, DefaultEventsMap, any>,
+  api: RSKExplorerAPI,
+  cmc: CoinMarketCapAPI,
+  address: string,
+  convert: string) => async () => {
+  const addresses = (await api.getTokensByAddress(address.toLowerCase()))
+    .map(token => token.contractAddress.toLocaleLowerCase())
+  const prices = await cmc.getQuotesLatest({ addresses, convert })
+  socket.emit('change', { type: 'newPrice', payload: prices })
+}
+
+export default pushNewPrices
