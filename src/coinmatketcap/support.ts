f68710eb30026f6287067efb13fb639020e4483d
Sleyter Sandoval <sleyter.sandoval@iovlabs.org>
2021-12-23 10:01:07 -0500

    Extends map between contract address and coin market cap

--- src/coinmatketcap/support.ts
+++ src/coinmatketcap/support.ts
@@ -1,6 +1,9 @@
 export const addressToCoinmarketcapId = {
   '0x0000000000000000000000000000000000000000': '3626', // RBTC
-  '0x2acc95758f8b5f583470ba265eb685a8f45fc9d5': '3701'
+  '0x2acc95758f8b5f583470ba265eb685a8f45fc9d5': '3701', // RIF
+  '0xef213441a85df4d7acbdae0cf78004e1e486bb96': '825', // rUSDT
+  '0x4991516df6053121121274397a8c1dad608bc95b': '7785', // rBUND
+  '0xefc78fc7d48b64958315949279ba181c2114abbd': '8669' // SOV
 }
 
 export const supportedFiat = ['USD']
