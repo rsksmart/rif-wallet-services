4d5b7a65234d78efe074dfc9e1a16d99a7db0118
Agustin Villalobos <agustin.villalobos@iovlabs.org>
2022-01-03 13:24:57 -0600

    exporting isConvertSupported function

--- src/coinmatketcap/validations.ts
+++ src/coinmatketcap/validations.ts
@@ -1,7 +1,7 @@
 import { addressToCoinmarketcapId, supportedFiat } from './support'
 
 export const isTokenSupported = (address: string, chainId: number) => addressToCoinmarketcapId[chainId][address] !== undefined
-const isConvertSupported = (convert: string) => supportedFiat.includes(convert)
+export const isConvertSupported = (convert: string) => supportedFiat.includes(convert)
 
 export const validatePricesRequest = (addresses: string[], convert: string, chainId: number) => {
   addresses.forEach(address => { if (!isTokenSupported(address, chainId)) throw new Error('Token address not supported') })
