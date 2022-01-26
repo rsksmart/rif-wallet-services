7a16bf6c3dd4fa887d2d7fab5c25859810077047
Agustín Villalobos <32603375+agustin-v@users.noreply.github.com>
2022-01-26 09:22:02 -0600

    Update src/middleware/index.ts
    Co-authored-by: Ilan <36084092+ilanolkies@users.noreply.github.com>

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -2,7 +2,7 @@ import { ErrorRequestHandler } from 'express'
 
 export class CustomError extends Error {
   status: number;
-  constructor (status: number, message: string) {
+  constructor (message: string, status: number) {
     super(message)
     this.status = status
   }
