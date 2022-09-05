8ae45b9e8af30406944c30146b204adff57870b0
Francis Rodriguez <francisngg@gmail.com>
2022-09-05 07:49:32 -0400

    Commented out mnemonic as it's not being used (the vpub was generated from that mnemonic)

--- test/BitcoinCore.test.ts
+++ test/BitcoinCore.test.ts
@@ -1,6 +1,6 @@
 import BitcoinCore from '../src/service/bitcoin/BitcoinCore'
-// eslint-disable-next-line @typescript-eslint/no-unused-vars,no-unused-vars, max-len
-const randomMnemonic = 'creek joy sea brain gravity execute month two voyage process bind coffee ecology body depend artwork erode punch episode unfair alpha amount cart clip'
+// eslint-disable-next-line max-len
+// const randomMnemonic = 'creek joy sea brain gravity execute month two voyage process bind coffee ecology body depend artwork erode punch episode unfair alpha amount cart clip'
 // eslint-disable-next-line max-len
 const vpub = 'vpub5Y3owbd2JX4bzwgH4XS5RSRzSnRMX6NYjqkd31sJEB5UGzqkq1v7iASC8R6vbxCWQ1xDDCm63jecwx3fkmv8FWHH5KeQeUyesrdJithe54K'
 describe('BitcoinCore unit tests', () => {
