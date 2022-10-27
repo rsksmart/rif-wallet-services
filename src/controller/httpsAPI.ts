9d8c8b65dd728da4daf021c229a4e1673bc94ac9
Sleyter Sandoval <sleyter.sandoval@iovlabs.org>
2022-10-26 21:24:34 -0500

    Exclude auth middleware for api documentation

--- src/controller/httpsAPI.ts
+++ src/controller/httpsAPI.ts
@@ -31,7 +31,7 @@ export class HttpsAPI {
   }
 
   init () : void {
-    this.app.use(this.authMiddleware)
+    this.app.use(/\/((?!api-docs).)*/, this.authMiddleware)
 
     this.app.get('/tokens', ({ query: { chainId = '31' } }: Request, res: Response, next: NextFunction) => this
       .dataSourceMapping[chainId as string].getTokens()
