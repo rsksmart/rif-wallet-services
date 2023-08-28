51821ecbba7c8675e9c6712a1f32c6219e7a6533
Sleyter Sandoval <sleyter.sandoval@iovlabs.org>
2023-08-28 09:43:37 -0500

    Price for USDRIF

--- src/service/price/mockPrice.ts
+++ src/service/price/mockPrice.ts
@@ -8,8 +8,8 @@ export class MockPrice extends PriceSupplier {
     '0x2d919f19d4892381d58edebeca66d5642cef1a1f', // RDOC
     // TestNet
     '0xc3de9f38581f83e281f260d0ddbaac0e102ff9f8', // RDOC
-    // MainNet - To confirm
-    '0x8dbf326e12a9ff37ed6ddf75ada548c2640a6482', //USDRIF
+    // MainNet
+    '0x8dbf326e12a9ff37ed6ddf75ada548c2640a6482', // USDRIF
     // TestNet
     '0x8dbf326e12a9ff37ed6ddf75ada548c2640a6482' // USDRIF
   ]
