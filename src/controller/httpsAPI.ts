49a0520f768fd7016a717577289670f7b7402b44
Sleyter Sandoval <sleyter.sandoval@iovlabs.org>
2023-09-05 18:44:21 -0500

    Adding validation for address and chainId

--- src/controller/httpsAPI.ts
+++ src/controller/httpsAPI.ts
@@ -10,6 +10,8 @@ import BitcoinRouter from '../service/bitcoin/BitcoinRouter'
 import { fromApiToRtbcBalance } from '../rskExplorerApi/utils'
 import { isMyTransaction } from '../service/transaction/utils'
 import { IApiTransactions, IEvent, IInternalTransaction } from '../rskExplorerApi/types'
+import { ValidationError, object, string } from 'yup'
+import { utils } from 'ethers'
 
 export class HttpsAPI {
   private app: Application
@@ -30,76 +32,121 @@ export class HttpsAPI {
     return res.status(200).json.bind(res)
   }
 
+  handleValidationError (e, res: Response) : void {
+    if (e instanceof ValidationError) {
+      res.status(400).json({ errors: e.errors })
+    } else {
+      throw e
+    }
+  }
+
   init () : void {
-    this.app.get('/tokens', ({ query: { chainId = '31' } }: Request, res: Response, next: NextFunction) => this
-      .dataSourceMapping[chainId as string].getTokens()
-      .then(this.responseJsonOk(res))
-      .catch(next)
-    )
+    const chainIdSchema = object({
+      chainId: string().optional()
+        .trim()
+        .oneOf(Object.keys(this.dataSourceMapping), 'The current chainId is not supported')
+    })
+    const addressSchema = object({
+      address: string().required('we required a valid address')
+        .trim()
+        .transform(address => utils.isAddress(address.toLowerCase()) ? address : '')
+    }).required()
+
+    this.app.get('/tokens', ({ query: { chainId = '31' } }: Request, res: Response, next: NextFunction) => {
+      try {
+        chainIdSchema.validateSync({ chainId })
+        return this
+          .dataSourceMapping[chainId as string].getTokens()
+          .then(this.responseJsonOk(res))
+          .catch(next)
+      } catch (e) {
+        this.handleValidationError(e, res)
+      }
+    })
 
     this.app.get(
       '/address/:address/tokens',
       async ({ params: { address }, query: { chainId = '31' } }: Request, res: Response, next: NextFunction) => {
-        const balance = await this.providerMapping[chainId as string].getBalance(address.toLowerCase())
-        Promise.all([
-          this.dataSourceMapping[chainId as string].getTokensByAddress(address),
-          fromApiToRtbcBalance(balance.toHexString(), parseInt(chainId as string))
-        ])
-          .then(balances => [...balances[0], balances[1]])
-          .then(this.responseJsonOk(res))
-          .catch(next)
+        try {
+          chainIdSchema.validateSync({ chainId })
+          addressSchema.validateSync({ address })
+          const balance = await this.providerMapping[chainId as string].getBalance(address.toLowerCase())
+          return Promise.all([
+            this.dataSourceMapping[chainId as string].getTokensByAddress(address),
+            fromApiToRtbcBalance(balance.toHexString(), parseInt(chainId as string))
+          ])
+            .then(balances => [...balances[0], balances[1]])
+            .then(this.responseJsonOk(res))
+            .catch(next)
+        } catch (e) {
+          this.handleValidationError(e, res)
+        }
       }
     )
 
     this.app.get(
       '/address/:address/events',
-      ({ params: { address }, query: { chainId = '31' } }: Request, res: Response, next: NextFunction) => this
-        .dataSourceMapping[chainId as string].getEventsByAddress(address)
-        .then(this.responseJsonOk(res))
-        .catch(next)
+      ({ params: { address }, query: { chainId = '31' } }: Request, res: Response, next: NextFunction) => {
+        try {
+          chainIdSchema.validateSync({ chainId })
+          addressSchema.validateSync({ address })
+          return this
+            .dataSourceMapping[chainId as string].getEventsByAddress(address)
+            .then(this.responseJsonOk(res))
+            .catch(next)
+        } catch (e) {
+          this.handleValidationError(e, res)
+        }
+      }
     )
 
     this.app.get(
       '/address/:address/transactions',
       async ({ params: { address }, query: { limit, prev, next, chainId = '31', blockNumber = '0' } }: Request,
         res: Response, nextFunction: NextFunction) => {
-        const dataSource = this.dataSourceMapping[chainId as string]
-        /* A transaction has the following structure { to: string, from: string }
-        * and to or from params should be our address when we send or receive a cryptocurrency
-        * (such as RBTC).
-        */
-        const transactions: {data: IApiTransactions[], prev: string, next: string} =
-        await dataSource.getTransactionsByAddress(address, limit as string,
-          prev as string, next as string, blockNumber as string)
-          .catch(nextFunction)
-        /* We query events to find transactions when we send or receive a token(ERC20)
-        * such as RIF,RDOC
-        * Additionally, we query internal transactions because we could send or receive a cryptocurrency
-        * invoking a smart contract.
-        * Finally, we filter by blocknumber and duplicates
-        */
-        const hashes: string[] = await Promise.all([dataSource.getEventsByAddress(address, limit as string),
-          dataSource.getInternalTransactionByAddress(address, limit as string)])
-          .then((promises) =>
-            promises.flat()
-              .filter((value: IEvent | IInternalTransaction) =>
-                isMyTransaction(value, address) && value.blockNumber >= +blockNumber)
-              .filter((value: IEvent | IInternalTransaction) => !transactions.data.map(tx => tx.hash)
-                .includes(value.transactionHash))
-              .map((value: IEvent | IInternalTransaction) => value.transactionHash)
+        try {
+          chainIdSchema.validateSync({ chainId })
+          addressSchema.validateSync({ address })
+          const dataSource = this.dataSourceMapping[chainId as string]
+          /* A transaction has the following structure { to: string, from: string }
+          * and to or from params should be our address when we send or receive a cryptocurrency
+          * (such as RBTC).
+          */
+          const transactions: {data: IApiTransactions[], prev: string, next: string} =
+          await dataSource.getTransactionsByAddress(address, limit as string,
+            prev as string, next as string, blockNumber as string)
+            .catch(nextFunction)
+          /* We query events to find transactions when we send or receive a token(ERC20)
+          * such as RIF,RDOC
+          * Additionally, we query internal transactions because we could send or receive a cryptocurrency
+          * invoking a smart contract.
+          * Finally, we filter by blocknumber and duplicates
+          */
+          const hashes: string[] = await Promise.all([dataSource.getEventsByAddress(address, limit as string),
+            dataSource.getInternalTransactionByAddress(address, limit as string)])
+            .then((promises) =>
+              promises.flat()
+                .filter((value: IEvent | IInternalTransaction) =>
+                  isMyTransaction(value, address) && value.blockNumber >= +blockNumber)
+                .filter((value: IEvent | IInternalTransaction) => !transactions.data.map(tx => tx.hash)
+                  .includes(value.transactionHash))
+                .map((value: IEvent | IInternalTransaction) => value.transactionHash)
+            )
+            .then((hashes: string[]) => Array.from(new Set(hashes)))
+            .catch(() => [])
+          const result = await Promise.all(
+            hashes.map((hash: string) => dataSource.getTransaction(hash))
           )
-          .then((hashes: string[]) => Array.from(new Set(hashes)))
-          .catch(() => [])
-        const result = await Promise.all(
-          hashes.map((hash: string) => dataSource.getTransaction(hash))
-        )
-        return Promise.resolve({
-          prev: transactions.prev,
-          next: transactions.next,
-          data: [...transactions.data, ...result]
-        })
-          .then(this.responseJsonOk(res))
-          .catch(nextFunction)
+          return Promise.resolve({
+            prev: transactions.prev,
+            next: transactions.next,
+            data: [...transactions.data, ...result]
+          })
+            .then(this.responseJsonOk(res))
+            .catch(nextFunction)
+        } catch (e) {
+          this.handleValidationError(e, res)
+        }
       }
 
     )
